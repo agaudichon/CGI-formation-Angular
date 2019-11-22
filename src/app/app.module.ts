1643598980b9d94972e23dfb1f253cfba1f5c5b8
alexandre.gaudichon <alexandre.gaudichon@cgi.com>
2019-11-22 16:25:21 +0100

    Clean arborescence

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppComponent} from './app.component';
-import {Tp1Component} from "./component/tp1/tp1.component";
+import {Tp1Component} from "./components/tp1/tp1.component";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, NgbModule],
