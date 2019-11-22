99ed3b1f2d7c5bfe9b60d2f218a22c5f9a5ac317
alexandre.gaudichon <alexandre.gaudichon@cgi.com>
2019-11-22 18:09:08 +0100

    Clean master

--- src/model/player.model.d.ts
+++
@@ -1,7 +0,0 @@
-export class Player {
-  id?: number;
-  firstname: string;
-  lastname: string;
-  jersey?: number;
-  teamId?: number;
-}
--- src/model/team.model.d.ts
+++
@@ -1,8 +0,0 @@
-export class Team {
-  id: number;
-  abbreviation: string;
-  fullname: string;
-  name: string;
-  location: string;
-  conference: string;
-}
