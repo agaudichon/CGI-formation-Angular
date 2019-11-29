4bce58acb782ec04ca44c1301b31eaef520cac5e
alexandre.gaudichon <alexandre.gaudichon@cgi.com>
2019-11-29 15:57:08 +0100

    Fix services

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,4 +1,5 @@
-import {Inject, Injectable} from '@angular/core';
+import {Injectable, Inject} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 import {Player} from '../../model/player.model';
@@ -8,21 +9,24 @@ import {ConfigurationToken} from "@app/configuration/configuration.token";
 export class PlayerService {
 
   private readonly baseUrl: string;
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
-  constructor(@Inject(ConfigurationToken) appConfiguration: any) {
+  constructor(@Inject(ConfigurationToken) appConfiguration: any, private http: HttpClient) {
     this.baseUrl = appConfiguration.fakeServerUrl;
   }
 
   public getPlayers(): Observable<Player[]> {
-    return null;
+    return this.http.get<Player[]>(this.baseUrl + '/players', this.httpOptions);
   }
 
   public getPlayer(id: number): Observable<Player> {
-    return null;
+    return this.http.get<Player>(this.baseUrl + '/players/' + id, this.httpOptions)
   }
 
   public addPlayer(player: Player): Observable<any> {
-    return null;
+    return this.http.post<Player>(this.baseUrl + '/players/', player, this.httpOptions)
   }
 
 }
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,24 +1,28 @@
-import {Inject, Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import {Team} from '../../model/team.model';
 import {ConfigurationToken} from "@app/configuration/configuration.token";
 
 @Injectable()
 export class TeamService {
-  
-  private readonly baseUrl: string;
 
-  constructor(@Inject(ConfigurationToken) appConfiguration: any) {
+  private readonly baseUrl: string;
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(@Inject(ConfigurationToken) appConfiguration: any, private http: HttpClient) {
     this.baseUrl = appConfiguration.fakeServerUrl;
   }
 
   public getTeams(): Observable<Team[]> {
-    return null;
+    return this.http.get<Team[]>(this.baseUrl + '/teams', this.httpOptions);
   }
 
   public getTeam(id: number): Observable<Team> {
-    return null;
+    return this.http.get<Team>(this.baseUrl + '/teams/' + id, this.httpOptions)
   }
 
 }
